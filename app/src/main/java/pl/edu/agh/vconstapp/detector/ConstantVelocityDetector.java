99deab401d1b9b786368a7be1572050de7058ec6
Julia Samól <juliasamol@Cleverland.local>
2018-05-08 10:24:20 +0200

    Create constant velocity detector

---
+++ app/src/main/java/pl/edu/agh/vconstapp/detector/ConstantVelocityDetector.java
@@ -0,0 +1,16 @@
+package pl.edu.agh.vconstapp.detector;
+
+import pl.edu.agh.vconstapp.data.DataPoint;
+import pl.edu.agh.vconstapp.data.Maneuver;
+import pl.edu.agh.vconstapp.util.ManeuverRepository;
+import pl.edu.agh.vconstapp.util.MultiStreamDataProvider;
+
+public class ConstantVelocityDetector implements ManeuverDetector {
+
+    @Override
+    public void update(MultiStreamDataProvider<DataPoint> dataPointsMultiStream,
+                       MultiStreamDataProvider<Maneuver> maneuversMultiStream,
+                       ManeuverRepository repository) {
+
+    }
+}
