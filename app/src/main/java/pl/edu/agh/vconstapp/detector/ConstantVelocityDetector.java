e57f83b423237e3adca5638e49ffd301ee804c2d
jsamol <julia.samol@gmail.com>
2018-05-09 23:26:21 +0200

    Detect constant velocity based on location data

--- app/src/main/java/pl/edu/agh/vconstapp/detector/ConstantVelocityDetector.java
+++ app/src/main/java/pl/edu/agh/vconstapp/detector/ConstantVelocityDetector.java
@@ -1,5 +1,11 @@
 package pl.edu.agh.vconstapp.detector;
 
+import android.util.Pair;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
 import pl.edu.agh.vconstapp.data.DataPoint;
 import pl.edu.agh.vconstapp.data.Maneuver;
 import pl.edu.agh.vconstapp.util.ManeuverRepository;
@@ -14,31 +20,112 @@ public class ConstantVelocityDetector implements ManeuverDetector {
     private static final String GPS_LOCATION_TAG = "GPS_LOCATION";
     private static final String ACCELEROMETER_TAG = "ACCELEROMETER";
 
+    private static final String DEVICE_MOCK_ID = "device_id";
+
+    private static final String VELOCITY_TAG = "s";
+    private static final String VELOCITY_ACCURACY_TAG = "s_acc";
+
+    private static final double ACCELERATION_ERROR = 0.5; // guessed number
+
+    private final List<Maneuver> mDetectedManeuvers;
+
+    private Pair<Double, Date> mLastConstantVelocityAndStartTimestamp;
+    private double mAcceleration;
+
+    public ConstantVelocityDetector() {
+        mDetectedManeuvers = new LinkedList<>();
+        resetSavedLastConstantVelocity();
+    }
+
     @Override
     public void update(MultiStreamDataProvider<DataPoint> dataPointsMultiStream,
                        MultiStreamDataProvider<Maneuver> maneuversMultiStream,
                        ManeuverRepository repository) {
 
-        Maneuver detectedManeuver = null;
+        resetDetectedManeuvers();
 
         if (dataPointsMultiStream.containsStreamTag(FUSED_LOCATION_TAG)) {
-            detectedManeuver = getManeuverFromLocation(dataPointsMultiStream.getStream(FUSED_LOCATION_TAG));
+            detectManeuversFromLocation(dataPointsMultiStream.getStream(FUSED_LOCATION_TAG));
         } else if (dataPointsMultiStream.containsStreamTag(GPS_LOCATION_TAG)) {
-            detectedManeuver = getManeuverFromLocation(dataPointsMultiStream.getStream(GPS_LOCATION_TAG));
+            detectManeuversFromLocation(dataPointsMultiStream.getStream(GPS_LOCATION_TAG));
         } else if (dataPointsMultiStream.containsStreamTag(ACCELEROMETER_TAG)) {
-            detectedManeuver = getManeuverFromAccelerometer(dataPointsMultiStream.getStream(ACCELEROMETER_TAG));
+            detectManeuverFromAccelerometer(dataPointsMultiStream.getStream(ACCELEROMETER_TAG));
         }
 
-        if (detectedManeuver != null) {
-            repository.addManeuver(detectedManeuver);
+        if (mDetectedManeuvers.size() > 0) {
+            for (Maneuver detectedManeuver : mDetectedManeuvers) {
+                repository.addManeuver(detectedManeuver);
+            }
         }
     }
 
-    private Maneuver getManeuverFromLocation(StreamDataProvider<DataPoint> locationStream) {
-        return null;
+    private void detectManeuversFromLocation(StreamDataProvider<DataPoint> locationStream) {
+        final int lastSeenIndex = locationStream.lastSeen();
+        if (lastSeenIndex > 0) {
+            startLocationDataAnalysisFromIndex(locationStream, lastSeenIndex);
+        } else {
+            resetSavedLastConstantVelocity();
+            startLocationDataAnalysisFromIndex(locationStream, 0);
+        }
     }
 
-    private Maneuver getManeuverFromAccelerometer(StreamDataProvider<DataPoint> accelerometerStream) {
-        return null;
+    private void startLocationDataAnalysisFromIndex(StreamDataProvider<DataPoint> locationStream, int index) {
+        final int streamSize = locationStream.size();
+
+        DataPoint earlierDataPoint;
+        DataPoint laterDataPoint;
+        for (int i = index; i < streamSize - 1; i++) {
+            earlierDataPoint = locationStream.get(i);
+            laterDataPoint = locationStream.get(i + 1);
+            checkIfConstantVelocityAndUpdateState(getAccelerationBetweenTwoDataPoints(earlierDataPoint, laterDataPoint),
+                                                  earlierDataPoint);
+        }
+    }
+
+    private void detectManeuverFromAccelerometer(StreamDataProvider<DataPoint> accelerometerStream) {
+    }
+
+    private double getAccelerationBetweenTwoDataPoints(DataPoint earlierDataPoint, DataPoint laterDataPoint) {
+        final double earlierVelocity = Double.valueOf(earlierDataPoint.getValue(VELOCITY_TAG));
+        final double laterVelocity = Double.valueOf(laterDataPoint.getValue(VELOCITY_TAG));
+        final long time = getDifferenceBetweenDatesInSeconds(earlierDataPoint.getTimestamp(), laterDataPoint.getTimestamp());
+
+        return calculateAcceleration(earlierVelocity, laterVelocity, time);
+    }
+
+    private double calculateAcceleration(double v1, double v2, long time) {
+        return (v2 - v1) / time;
+    }
+
+    private long getDifferenceBetweenDatesInSeconds(Date earlierDate, Date laterDate) {
+        long difference = laterDate.getTime() - earlierDate.getTime();
+        return (long) (difference * 0.001);
+    }
+
+    private void checkIfConstantVelocityAndUpdateState(double acceleration, DataPoint dataPoint) {
+        if (Math.abs(acceleration - this.mAcceleration) <= ACCELERATION_ERROR) {
+            updateAccelerationAndLastConstantVelocity(acceleration, dataPoint);
+        } else {
+            mDetectedManeuvers.add(new Maneuver(DEVICE_MOCK_ID,
+                                                mLastConstantVelocityAndStartTimestamp.second,
+                                                dataPoint.getTimestamp(),
+                                                CONSTANT_VELOCITY_MANEUVER_TAG,
+                                                null)); // TODO: add params
+        }
+    }
+
+    private void resetDetectedManeuvers() {
+        this.mDetectedManeuvers.clear();
+    }
+
+    private void resetSavedLastConstantVelocity() {
+        this.mLastConstantVelocityAndStartTimestamp = null;
+    }
+
+    private void updateAccelerationAndLastConstantVelocity(double acceleration, DataPoint dataPoint) {
+        this.mAcceleration = acceleration;
+        this.mLastConstantVelocityAndStartTimestamp =
+                new Pair<>(Double.valueOf(dataPoint.getValue(VELOCITY_TAG)),
+                           dataPoint.getTimestamp());
     }
 }
